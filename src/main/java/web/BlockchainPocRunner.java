fd8fe1f343902e4e3acda890ccc4dab8f391730c
Akhilesh Tripathi <akhileshtripa@gmail.com>
2019-12-27 10:46:30 +0530

    update image path

---
+++ src/main/java/web/BlockchainPocRunner.java
@@ -0,0 +1,12 @@
+package web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainPocRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchainPocRunner.class, args);
+    }
+}
